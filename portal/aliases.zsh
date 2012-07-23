a27f481641d8b658a63d8fe78e5e442ee58bb91b
Scott Jungling <sjungling@csuchico.edu>
2012-07-23 12:42:27 -0700

    fixes #1 by subbing out full path for $HOME

--- portal/aliases.zsh
+++ portal/aliases.zsh
@@ -1,3 +1,4 @@
 alias portalstart="$HOME/Projects/portal-tools/tomcat-latest/bin/catalina.sh start"
 alias portalstop="$HOME/Projects/portal-tools/tomcat-latest/bin/catalina.sh stop -force" 
+alias portalrestart="portalstop; portalstart"
 
