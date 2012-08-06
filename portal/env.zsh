3597202081c32858425418bd5506419213bbe9ec
Scott Jungling <sjungling@csuchico.edu>
2012-08-06 14:22:33 -0700

    use $HOME instead of hard-coded path

--- portal/env.zsh
+++ portal/env.zsh
@@ -1,4 +1,4 @@
-export M2_HOME=/Users/sjungling/Projects/portal-tools/maven-latest
+export M2_HOME=$HOME/Projects/portal-tools/maven-latest
 export M2=$M2_HOME/bin
-export ANT_HOME=/Users/sjungling/Projects/portal-tools/ant-latest
+export ANT_HOME=$HOME/Projects/portal-tools/ant-latest
 export JAVA_HOME=`/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java_home`
