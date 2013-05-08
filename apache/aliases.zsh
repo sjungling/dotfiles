269487cb2d0ee0342aee5075927ae327eda4902a
Scott Jungling <scott.jungling@build.com>
2013-05-08 10:58:10 -0700

    Adding new aliases and updating configs

--- apache/aliases.zsh
+++ apache/aliases.zsh
@@ -1,1 +1,2 @@
 alias aprestart="sudo apachectl restart"
+alias cfrestart="/Applications/ColdFusion10/cfusion/bin/coldfusion restart"
--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,1 +1,3 @@
 alias "reload!"='source ~/.bashrc'
+alias "cb"="pbcopy"
+alias "corig"="find . -name '*.orig' -delete"
